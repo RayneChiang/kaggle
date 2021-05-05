0604347bb2d5fd6e234eebefb04bf1bc4c8fe4ff
Runying Jiang <raynechiang5235@gmail.com>
2021-05-05 04:16:14 +0100

    [something updated]

--- help_xyb/main.py
+++ help_xyb/main.py
@@ -1,21 +1,97 @@
 import os
 import pandas as pd
+import numpy as np
 import seaborn as sns
+from sklearn import tree
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import operator
+from sklearn.model_selection import train_test_split
+from xgboost import plot_tree
+
+
+def get_missing(df):
+    for col in df.columns.tolist():
+        df[col].fillna(round(df[col].mean()), inplace=True)
+        print('{} column missing values: {}'.format(col, df[col].isnull().sum()))
+    print('\n')
+
+def get_NaNout(df):
+    for col in df.columns.tolist():
+        index_list = df[col][df[col] == '”NaN”'].index
+        for i in index_list:
+            df.loc[i, col] = round(df[col].mean())
+
+def create_feature_map(features):
+    outfile = open('xgb.fmap', 'w')
+    for i, feat in enumerate(features):
+        outfile.write('{0}\t{1}\tq\n'.format(i, feat))
+    outfile.close()
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('1.csv')
-    # file = file.values
-    target = df[df['E2'] == ' ']
-    E1_frame = target[['E1_a', 'E1_b', 'E1_c', 'E1_d', 'E1_e', 'E1_f', 'E1_g', 'E1_h']]
+    df = pd.read_csv('data_A3a_dropna.csv')
+    columns = ['A3a', 'A2', 'A1', 'D1_a', 'D1_b', 'D1_c', 'D1_d', 'D1_e', 'D1_f',
+               'A5_a', 'A5_b', 'A5_c', 'A5_d', 'A5_e', 'A5_f', 'A5_g',
+               'A4_a', 'A4_b', 'A4_c', 'A4_d', 'A4_e', 'B5', 'B4', 'E14', 'p_age_group_sdc']
+    # columns =
+    df_select = df[df.columns[3:-7]]
+    get_missing(df_select)
+    # get_NaNout(df_select)
+    y = df['Mental_Goal']
+    X = df_select
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+    #
+    # xg_reg = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.7, learning_rate=0.1,
+    #                           max_depth=10, alpha=10, n_estimators=100)
+    # xg_reg.fit(X_train, y_train)
+    #
+    # preds = xg_reg.predict(X_test)
+    #
+    # rmse = np.sqrt(mean_squared_error(y_test, preds))
+    # print("RMSE: %f" % (rmse))
 
-    cols = ['E1_a', 'E1_b', 'E1_c', 'E1_d', 'E1_e', 'E1_f', 'E1_g', 'E1_h']
-    for col in cols:
-        index_list = E1_frame[col][E1_frame[col] == 1].index
-        for i in index_list:
-            df.loc[i, 'E2'] = col
 
-    left_frame = df[df['E2'] == ' ']
-    print(len(left_frame))
+    # xgb.plot_tree(xg_reg, num_trees=0)
+    # plt.rcParams['figure.figsize'] = [50, 10]
+    # plt.show()
 
-    df.to_csv('1_1.csv')
+
+
+    features = df.columns[3:-7]
+    params = {"objective": "reg:linear",
+              "booster": "gbtree",
+              "eta": 0.3,
+              "max_depth": 10,
+              "subsample": 0.9,
+              "colsample_bytree": 0.7,
+              "silent": 1,
+              "seed": 1301
+              }
+    num_boost_round = 300
+
+    dtrain = xgb.DMatrix(X, y)
+    gbm = xgb.train(params, dtrain, num_boost_round)
+
+    create_feature_map(features)
+    importance = gbm.get_score(importance_type='weight')
+    importance = sorted(importance.items(), key=operator.itemgetter(1))
+
+    df = pd.DataFrame(importance, columns=['feature', 'fscore'])
+    df['fscore'] = df['fscore'] / df['fscore'].sum()
+
+    featp = df.plot(kind='barh', x='feature', y='fscore', legend=False, figsize=(6, 10))
+    plt.title('XGBoost Feature Importance')
+    plt.xlabel('relative importance')
+    fig_featp = featp.get_figure()
+    fig_featp.savefig('feature_importance_xgb.png', bbox_inches='tight', pad_inches=1)
+
+    # keys = list(importance.keys())
+    # values = list(importance.values())
+    #
+    # data = pd.DataFrame(data=values, index=keys, columns=["score"]).sort_values(by="score", ascending=True)
+    # data.plot(kind='barh')
+    # plt.show()
--- ittv/data_with_python.py
+++ ittv/data_with_python.py
@@ -10,8 +10,9 @@ def find_most_common_post_type():
     Write a function that returns the most common type of post (string).
     """
     df = load_data()
-    
+
     # write your code here
+    return df.groupby('Type').size().idxmax()
 
 
 def is_average_for_paid_higher():
@@ -20,8 +21,11 @@ def is_average_for_paid_higher():
     higher for paid posts. The function should return a boolean (True / False).
     """
     df = load_data()
-    
+
     # write your code here
+    total_likes_on_cost = df.groupby('Paid')['Page total likes'].mean().idxmax()
+
+    return True if (total_likes_on_cost == 1.0) else False
 
 
 def determine_best_month_per_category():
@@ -31,5 +35,27 @@ def determine_best_month_per_category():
 
     """
     df = load_data()
-    
+
     # write your code here
+    return_list = []
+    category_list = sorted(df['Category'].unique())
+    for category in category_list:
+        df_month_per_category = df[df['Category'] == category].groupby(['Post Month'])[
+            'Page total likes'].max().idxmax()
+        return_list.append(df_month_per_category)
+
+    return return_list
+
+if __name__ == '__main__':
+    ######################################################
+    # Coding test
+    ######################################################
+    # test find_most_common_post_type
+    type_max = find_most_common_post_type()
+    assert (type_max == 'Photo')
+
+    # test is_average_for_paid_higher
+    assert (is_average_for_paid_higher() == True)
+
+    # test determine_best_month_per_category
+    assert (determine_best_month_per_category() == [12, 12, 12])
